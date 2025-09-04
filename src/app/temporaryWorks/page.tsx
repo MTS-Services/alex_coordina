4fcc201c40f33fe2fdb68dff0f3aceee0160931f
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2025-09-04 20:56:51 +0600

    (fix): added content in temporary page

--- src/app/temporaryWorks/page.tsx
+++ src/app/temporaryWorks/page.tsx
@@ -4,6 +4,7 @@ const page = () => {
   return (
     <div className='p-8' id='temporary-works-page'>
       <h1>Temporary Works Page</h1>
+      <p>This is the temporary works page.</p>
     </div>
   );
 };
